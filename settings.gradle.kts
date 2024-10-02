d78e2f25a688f470e434e90b11cef282e6b3ed7f
Costantino Paciello <114727913+cost-02@users.noreply.github.com>
2024-10-02 15:48:47 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "question6_6788018"
+include(":app")
+ 
